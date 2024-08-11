2e21effd585134308890113b94bd26317a8815c9
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2024-08-11 12:34:16 +0530

    month component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import Month from './components/Month';
+
 function App() {
   return (
     <div className="App">
-      <h1 class="text-xl font-medium text-black">Text</h1>
+      <h1 className="text-xl font-medium text-black">Text</h1>
+      <Month month="January" days="30"/>
     </div>
   );
 }
---
+++ src/components/Month.js
@@ -0,0 +1,31 @@
+import { daysOfWeek } from '../utils/constants';
+
+const Month = (props) => {
+    const { month, days } = props;
+    return (
+        <div className='w-56 h-[252px] pt-0 pr-3.5 pb-4 pl-5'>
+            <h2 className='text-base font-medium text-gray-500 ml-2 h-8'>{month}</h2>
+            <div>
+                {
+                    daysOfWeek.map((day, index) => (
+                        <div key={index} className=' text-gray-600 inline-block relative h-6 w-6 rounded-full'>
+                            <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xs'>{day.charAt(0)}</span>
+                        </div>
+                    ))
+                }
+            </div>
+            <div>
+                {
+                    Array.from({ length: days }, (_, index) => index + 1).map((num) => (
+                        <div key={num} className=' text-gray-600 inline-block relative h-6 w-6 rounded-full hover:bg-slate-200 cursor-pointer'>
+                            <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[10px]'>{num}</span>
+                        </div>
+                    )
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Month;
